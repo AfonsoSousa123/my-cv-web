59c55f9d16f06209a493e3885248293783396f7e
AfonsoSousa123 <afonsosousa10@gmail.com>
2023-07-13 12:15:44 +0100

    Update 1.0 (Laid out the template of the SPA)

---
+++ src/components/Loader.tsx
@@ -0,0 +1,12 @@
+const Loader = () => {
+  return (
+    <>
+      <section className="preloader">
+        <div className="spinner">
+          <span className="spinner-rotate"></span>
+        </div>
+      </section>
+    </>
+  );
+};
+export default Loader;
