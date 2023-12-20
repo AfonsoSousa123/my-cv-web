dcf696996c85e54334c349b97220a9d62ccdff29
AfonsoSousa123 <afonsosousa10@gmail.com>
2023-12-20 10:57:52 +0000

    Update 1.9 (trying to correct a production error)

--- src/main.tsx
+++ src/main.tsx
@@ -9,12 +9,13 @@ import "./assets/css/templatemo-first-portfolio-style.css";
 import "./assets/css/bootstrap-icons.css";
 
 // JS files
-// import "./assets/js/jquery.min.js";
-// import "./assets/js/bootstrap.min.js";
-// import "./assets/js/custom.js";
-// import "./assets/js/jquery.magnific-popup.min.js";
-// import "./assets/js/jquery.sticky.js";
-// import "./assets/js/magnific-popup-options.js";
+import "./assets/js/jquery.min.js";
+import "./assets/js/bootstrap.min.js";
+import "./assets/js/jquery.sticky.js";
+// import "./assets/js/click-scroll.js";
+import "./assets/js/jquery.magnific-popup.min.js";
+import "./assets/js/magnific-popup-options.js";
+import "./assets/js/custom.js";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
