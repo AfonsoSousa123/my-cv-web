f3fae304b8bc244f047f5cea40c2dc0b7241bed4
AfonsoSousa123 <afonsosousa10@gmail.com>
2025-02-18 09:33:06 +0000

    fix: Fixed navbar functionality with toggle and responsive behavior

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,80 +1,86 @@
+import { useState, useEffect } from "react";
+
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 991) {
+        setIsMenuOpen(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <>
-      <nav className="navbar navbar-expand-lg">
-        <div className="container">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
+      <>
+        <nav className="navbar navbar-expand-lg">
+          <div className="container">
+            <button
+                className="navbar-toggler"
+                type="button"
+                onClick={toggleMenu}
+                aria-controls="navbarNav"
+                aria-expanded={isMenuOpen}
+                aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
 
-          <a href="/" className="navbar-brand mx-auto mx-lg-0">
-            CV Portfolio
-          </a>
+            <a href="/" className="navbar-brand mx-auto mx-lg-0">
+              CV Portfolio
+            </a>
 
-          <div className="d-flex align-items-center d-lg-none">
-            <a className="navbar-icon bi-telephone-plus me-3" href="#section_contact"></a>
-          </div>
+            <div className={`collapse navbar-collapse ${isMenuOpen ? "show" : ""}`} id="navbarNav">
+              <ul className="navbar-nav ms-lg-5">
+                <li className="nav-item">
+                  <a className="nav-link click-scroll" href="#section_1">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link click-scroll" href="#section_about">
+                    About
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link click-scroll" href="#section_skills">
+                    Skills
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link click-scroll" href="#section_companies">
+                    Companies
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link click-scroll" href="#section_projects">
+                    Projects
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link click-scroll" href="#section_contact">
+                    Contact
+                  </a>
+                </li>
+              </ul>
 
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ms-lg-5">
-              <li className="nav-item">
-                <a className="nav-link click-scroll" href="#section_1">
-                  Home
-                </a>
-              </li>
-
-              <li className="nav-item">
-                <a className="nav-link click-scroll" href="#section_about">
-                  About
-                </a>
-              </li>
-
-              <li className="nav-item">
-                <a className="nav-link click-scroll" href="#section_skills">
-                  Skills
-                </a>
-              </li>
-
-              <li className="nav-item">
-                <a className="nav-link click-scroll" href="#section_companies">
-                  Companies
-                </a>
-              </li>
-
-              {/*<li className="nav-item">*/}
-              {/*  <a className="nav-link click-scroll" href="#section_services">*/}
-              {/*    Services*/}
-              {/*  </a>*/}
-              {/*</li>*/}
-
-              <li className="nav-item">
-                <a className="nav-link click-scroll" href="#section_projects">
-                  Projects
-                </a>
-              </li>
-
-              <li className="nav-item">
-                <a className="nav-link click-scroll" href="#section_contact">
-                  Contact
-                </a>
-              </li>
-            </ul>
-
-            <div className="d-lg-flex align-items-center d-none ms-auto">
-              <a className="navbar-icon bi-telephone-plus icon-link-hover me-3" href="#section_contact"></a>
+              <div className="d-lg-flex align-items-center d-none ms-auto">
+                <a className="navbar-icon bi-telephone-plus icon-link-hover me-3" href="#section_contact"></a>
+              </div>
             </div>
           </div>
-        </div>
-      </nav>
-    </>
+        </nav>
+      </>
   );
 };
+
 export default Header;
